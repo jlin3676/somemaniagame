8cdb3092c4e2a1e1c27f5454adbc2065fcaa429b
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2018-01-18 13:08:56 -0500

    zl

--- Mania/src/thegame/KeyStroke.java
+++ Mania/src/thegame/KeyStroke.java
@@ -1,11 +1,37 @@
 package thegame;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class KeyStroke {
+public class KeyStroke extends Application {
 
-	long timeStep = System.nanoTime() + 10000000000L;
-	
-	
-	public void key() {
-		
-	}
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BorderPane pane = new BorderPane();
+        Button button = new Button("Press Me!");
+        pane.setCenter(button);
+        Scene scene = new Scene(pane, 200, 200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        button.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.ENTER) {
+                    System.out.println("Enter Pressed");
+                }
+                if(event.getCode())
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
